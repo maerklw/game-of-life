be8ba0a98d007983039aa0f48c78c3c97dfb6e74
Werner Märkl <wmaerkl72@gmail.com>
2020-02-11 14:44:53 +0100

    zurück zum original * and .

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -9,7 +9,7 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
 	// Symbols to represent cell status
-    LIVE_CELL("1"), DEAD_CELL("0");
+    LIVE_CELL("*"), DEAD_CELL(".");
     // better solution
 
     private String symbol;
